e11a5ce620f3102ba509f2a0915ef51d131683a7
Myxic <105291867+Myxic@users.noreply.github.com>
2022-10-15 00:41:03 +0100

    important

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,16 +1,96 @@
-const buttons = document.querySelectorAll("[data-carousel-button]");
+const rowdiv = document.querySelectorAll(".row div")
 
-buttons.forEach(button => {
-  
-    button.addEventListener("click", ()=>{
-        const offset = button.dataset.carouselButton === "next" ? 1 : -1
-        const slides = button.closest("[data-carousel]").querySelector("[data-slides]")
-        const activeSlide = slides.querySelector("[data-active]")
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset
-        if (newIndex < 0) newIndex = slides.children.length -1
-        if (newIndex >= slides.children.length) newIndex = 0
 
-        slides.children[newIndex].dataset.active = true
-        delete activeSlide.dataset.active
-    })
+
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1;console.log(slideIndex)}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+var slideIndex = 1;
+(showSlides(slideIndex));
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+const next = document.getElementById("next");
+const prev = document.getElementById("prev")
+const row = document.getElementById("row")
+const thumbnail = document.getElementById("thumbnail")
+
+// To open the correct slideIndex when you click the img
+let num;
+row.addEventListener("click", function(e){
+    var target = e.target.closest("div");
+    var parent = target.parentNode;
+    const index = [].indexOf.call(parent.children, target)
+    num = index + 1
+    currentSlide(num)
+    
+
 })
+// for the thumbnail display
+thumbnail.addEventListener("click", function(e){
+  // console.log("hello")
+  // console.log(thumbnail)
+  var target = e.target.closest("div");
+  var parent = target.parentNode;
+  const index = [].indexOf.call(parent.children, target)
+  // console.log(index)
+  num = index + 1
+  
+  currentSlide(num)
+  
+  
+})
+// This is just to open the Modal
+rowdiv.forEach(div => {
+  
+  div.addEventListener("click", ()=>{
+    openModal()
+
+  })
+});
+
+
+const modalcontent = document.querySelectorAll(".modal-content img")
+
+
+// modalcontent.forEach(img => {
+//   img.addEventListener("touchmove", ()=>{
+//     plusSlides(+1)
+//   })
+
+// })
+// Element.setCapture()
+
+
