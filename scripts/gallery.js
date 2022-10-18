41c8b1c6078167f45e76b87e5a62b935675688ca
Ezemenahi Benedict <ezemenahi56@gmail.com>
2022-10-19 00:39:10 +0100

    done

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -13,14 +13,38 @@ function openModal() {
 function closeModal() {
   document.getElementById("myModal").style.display = "none";
 }
+// Open the Modal
+function openModal2() {
+  document.getElementById("myModal2").style.display = "block";
+}
+
+// Close the Modal
+function closeModal2() {
+  document.getElementById("myModal2").style.display = "none";
+}
+// Open the Modal
+function openModal3() {
+  document.getElementById("myModal3").style.display = "block";
+}
+
+// Close the Modal
+function closeModal3() {
+  document.getElementById("myModal3").style.display = "none";
+}
 
 // Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
+function currentSlide2(n) {
+  showSlides2(slideIndex = n);
+}
+function currentSlide3(n) {
+  showSlides3(slideIndex = n);
+}
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("mySlides1");
   var dots = document.getElementsByClassName("demo");
   var captionText = document.getElementById("caption");
   if (n > slides.length) { slideIndex = 1; console.log(slideIndex) }
@@ -36,12 +60,56 @@ function showSlides(n) {
   captionText.innerHTML = dots[slideIndex - 1].alt;
 }
 
+function showSlides2(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides2");
+  var dots = document.getElementsByClassName("demo2");
+  var captionText = document.getElementById("caption2");
+  if (n > slides.length) { slideIndex = 1; console.log(slideIndex) }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+
+function showSlides3(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides3");
+  var dots = document.getElementsByClassName("demo3");
+  var captionText = document.getElementById("caption3");
+  if (n > slides.length) { slideIndex = 1; console.log(slideIndex) }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+
 var slideIndex = 1;
 (showSlides(slideIndex));
+(showSlides2(slideIndex));
+(showSlides3(slideIndex));
 
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
+}
+function plusSlides2(n) {
+  showSlides2(slideIndex += n);
+}
+function plusSlides2(n) {
+  showSlides2(slideIndex += n);
 }
 
 const next = document.getElementById("next");
@@ -50,6 +118,8 @@ const row1 = document.getElementById("row-1")
 const row2 = document.getElementById("row-2")
 const row3 = document.getElementById("row-3")
 const thumbnail = document.getElementById("thumbnail")
+const thumbnail2 = document.getElementById("thumbnail2")
+const thumbnail3 = document.getElementById("thumbnail3")
 
 // To open the correct slideIndex when you click the img
 let num;
@@ -67,7 +137,7 @@ row2.addEventListener("click", function (e) {
   var parent = target.parentNode;
   const index = [].indexOf.call(parent.children, target)
   num = index + 1
-  currentSlide(num)
+  currentSlide2(num)
 
 
 })
@@ -76,7 +146,7 @@ row3.addEventListener("click", function (e) {
   var parent = target.parentNode;
   const index = [].indexOf.call(parent.children, target)
   num = index + 1
-  currentSlide(num)
+  currentSlide3(num)
 
 
 })
@@ -94,6 +164,32 @@ thumbnail.addEventListener("click", function (e) {
 
 
 })
+thumbnail2.addEventListener("click", function (e) {
+  // console.log("hello")
+  // console.log(thumbnail)
+  var target = e.target.closest("div");
+  var parent = target.parentNode;
+  const index = [].indexOf.call(parent.children, target)
+  // console.log(index)
+  num = index + 1
+
+  currentSlide2(num)
+
+
+})
+thumbnail3.addEventListener("click", function (e) {
+  // console.log("hello")
+  // console.log(thumbnail)
+  var target = e.target.closest("div");
+  var parent = target.parentNode;
+  const index = [].indexOf.call(parent.children, target)
+  // console.log(index)
+  num = index + 1
+
+  currentSlide3(num)
+
+
+})
 // This is just to open the Modal
 rowdiv1.forEach(div1 => {
 
@@ -105,15 +201,15 @@ rowdiv1.forEach(div1 => {
 rowdiv2.forEach(div2 => {
 
   div2.addEventListener("click", () => {
-    openModal()
+    openModal2()
 
   })
 });
-console.log(rowdiv3)
+
 rowdiv3.forEach(div3 => {
 
   div3.addEventListener("click", () => {
-    openModal()
+    openModal3()
 
   })
 });
