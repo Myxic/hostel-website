6b9ae215232db92bdeaf267b1137325eb601815d
Ezemenahi Benedict <ezemenahi56@gmail.com>
2022-10-24 20:10:32 +0100

    dark mode js

---
+++ scripts/student.js
@@ -0,0 +1,30 @@
+let darkMode = localStorage.getItem('darkMode');
+
+const darkModeTogle = document.querySelector("#check")
+
+const enableDarkMode = () => {
+    // 1. add the class darkmode to the body 
+    document.body.classList.add("darkmode");
+    // 2. update darkmode in the localStorage
+    localStorage.setItem('darkMode', 'enabled');
+}
+const disableDarkMode = () => {
+    // 1. add the class off darkmode to the body 
+    document.body.classList.remove("darkmode");
+    // 2. update darkmode in the localStorage
+    localStorage.setItem('darkMode', null);
+}  
+if (darkMode === 'enabled') {
+    enableDarkMode();
+}
+
+check.addEventListener("click", ()=> {
+    darkMode = localStorage.getItem('darkMode');
+    if (check.checked && darkMode !== "enabled"){
+        enableDarkMode();
+        // console.log(darkMode);
+    }else{
+        disableDarkMode();
+        // console.log(darkMode);
+    }
+});
