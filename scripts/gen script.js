56a15dbacd054b64ced36a78f99e61a89be89fd1
Ezemenahi Benedict <ezemenahi56@gmail.com>
2023-06-01 03:50:29 +0100

    fixes
    fixed hamburger menu

--- scripts/gen script.js
+++ scripts/gen script.js
@@ -15,30 +15,42 @@ close.onclick= function() {
     booking.style.display = "none";
     document.querySelector("nav figure").style.display = "block"
 };
-
-
-burger.addEventListener("click", ()=> {
+window.addEventListener("resize", ()=> {
+if (window.matchMedia("(max-width: 400px)").matches) {
+    // Viewport is less or equal to 400 pixels wide
+    burger.addEventListener("click", ()=> {
     
-    document.querySelector(".nav_ul").style.display = "block";
-    document.querySelector("#top").style.display = "none"
-    
+        document.querySelector(".nav_ul").classList.add("buger_nav_ul");
+        document.querySelector(".nav_ul").style.display = "block";
 
-    // if (document.querySelector(".nav_ul").style.display = "none") {
-    //     document.querySelector(".nav_ul").style.display = "block";
-    //     document.querySelector("#top").style.display = "none"
-    //     exit.style.display = "none"
-    // } else if (document.querySelector(".nav_ul").style.display = "block") {
-    //     document.querySelector(".nav_ul").style.display = "none";
-    // }
+        document.querySelector("#top").style.display = "none"
+
+        
+    
+        // if (document.querySelector(".nav_ul").style.display = "none") {
+        //     document.querySelector(".nav_ul").style.display = "block";
+        //     document.querySelector("#top").style.display = "none"
+        //     exit.style.display = "none"
+        // } else if (document.querySelector(".nav_ul").style.display = "block") {
+        //     document.querySelector(".nav_ul").style.display = "none";
+        // }
+    });
+    
+    exit.addEventListener("click", ()=> {
+        // document.querySelector(".nav_ul").style.right = "50vw"; 
+        // document.querySelector(".nav_ul").style.animationDirection= "reverse";
+        document.querySelector(".nav_ul").style.display = "none";
+        document.querySelector("#top").style.display = "block"
+        
+    })
+  } else {
+    // Viewport is greater than 400 pixels wide
+
+    
+    document.querySelector(".nav_ul").classList.remove("buger_nav_ul");
+    window.location.reload(false);
+  }
 });
-
-exit.addEventListener("click", ()=> {
-    // document.querySelector(".nav_ul").style.right = "50vw"; 
-    // document.querySelector(".nav_ul").style.animationDirection= "reverse";
-    document.querySelector(".nav_ul").style.display = "none";
-    document.querySelector("#top").style.display = "block"
-    
-})
 
 // window.addEventListener("resize", ()=> {
 //     if (document.querySelector(".nav_ul").position= "absolute"){
@@ -51,16 +63,4 @@ exit.addEventListener("click", ()=> {
 //     document.getElementById("demo").innerHTML = Math.random();
 //   });
 
-
-// https://jsfiddle.net/q82nhef0/
-
-// $("#thediv").click(function(e) {
-//     var divWidth = $("#thediv").width();        
-//     var clickX = e.clientX;
-//     if (clickX > divWidth/2) {
-//         console.log("Div was clicked on the right");
-//     } else {
-//         console.log("Div was clicked on the left");
-//     }
-// });
 
