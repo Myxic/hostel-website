a6cc296170fd4564fd5682e4be11ae228166c679
Ezemenahi Benedict <ezemenahi56@gmail.com>
2024-01-28 22:10:58 +0100

    upload

--- scripts/Gallery/Events.js
+++ scripts/Gallery/Events.js
@@ -1,8 +1,15 @@
-const ImageLinkName = [
-    "https://api.myjson.online/v1/records/eabbd3e1-179a-422c-8f1a-3cca07145fcf"
-]
-
-
 // const ImageLinkName = [
-//     "https://pastebin.com/raw/ShkXmdHg"
+//     "https://api.myjson.online/v1/records/eabbd3e1-179a-422c-8f1a-3cca07145fcf"
 // ]
+
+const ImageLinkName = 
+{
+    "Links": [
+      "https://res.cloudinary.com/dfk4z2ch2/image/upload/v1702984439/testing/k31fhrgleugztlew7svz.jpg",
+      "https://res.cloudinary.com/dfk4z2ch2/image/upload/v1702984402/testing/dzvk60lsrbdckbit5bhl.jpg"
+    ],
+    "Captions": [
+      "A Man",
+      "A King"
+    ]
+  };
--- scripts/Gallery/Exterior.js
+++ scripts/Gallery/Exterior.js
@@ -1,3 +1,15 @@
-const ImageLinkName = [
-    "https://api.myjson.online/v1/records/0c1ad868-7e0d-470a-8390-5974ddaf833c"
-]
+// const ImageLinkName = [
+//     "https://api.myjson.online/v1/records/0c1ad868-7e0d-470a-8390-5974ddaf833c"
+// ]
+
+const ImageLinkName = 
+{
+    "Links": [
+      "https://res.cloudinary.com/dfk4z2ch2/image/upload/v1703065532/HolyFamily/HFExterior/xpkezd9ldeyfod2k5ycj.jpg",
+      "https://res.cloudinary.com/dfk4z2ch2/image/upload/v1702984402/testing/dzvk60lsrbdckbit5bhl.jpg"
+    ],
+    "Captions": [
+      "Car Park",
+      "A King"
+    ]
+  };
