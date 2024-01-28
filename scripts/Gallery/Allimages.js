caacbb5e2de9fd2f418a6db2aa475b114b32022f
Ezemenahi Benedict <ezemenahi56@gmail.com>
2024-01-28 23:04:43 +0100

    upload

--- scripts/Gallery/Allimages.js
+++ scripts/Gallery/Allimages.js
@@ -19,13 +19,29 @@ const ImageLinkName = [
 
     {
         "Links": [
-          "https://res.cloudinary.com/dfk4z2ch2/image/upload/v1703065532/HolyFamily/HFExterior/xpkezd9ldeyfod2k5ycj.jpg",
-          "https://res.cloudinary.com/dfk4z2ch2/image/upload/v1702984402/testing/dzvk60lsrbdckbit5bhl.jpg"
-        ],
-        "Captions": [
-          "Car Park",
-          "A King"
-        ]
+            "https://res.cloudinary.com/dfk4z2ch2/image/upload/v1703065532/HolyFamily/HFExterior/xpkezd9ldeyfod2k5ycj.jpg",
+            "https://res.cloudinary.com/dfk4z2ch2/image/upload/v1706478110/HolyFamily/HFExterior/sx4nw6ikek5yn6kfh3li.jpg",
+            "https://res.cloudinary.com/dfk4z2ch2/image/upload/v1706478115/HolyFamily/HFExterior/m3mtfjcgozakr84jiijm.jpg",
+            "https://res.cloudinary.com/dfk4z2ch2/image/upload/v1706478117/HolyFamily/HFExterior/wh8ypvxcmyuelxm2e7lj.jpg",
+            "https://res.cloudinary.com/dfk4z2ch2/image/upload/v1706478112/HolyFamily/HFExterior/k4gdlzbhwtcjbxuki86k.jpg",
+            "https://res.cloudinary.com/dfk4z2ch2/image/upload/v1706478104/HolyFamily/HFExterior/dxfl7utyfjsvrgvxtz4a.jpg",
+            "https://res.cloudinary.com/dfk4z2ch2/image/upload/v1706478100/HolyFamily/HFExterior/oz7bioowimz5cck1nnv1.jpg",
+            "https://res.cloudinary.com/dfk4z2ch2/image/upload/v1706478095/HolyFamily/HFExterior/vffosz1t7nw1mjnk0e6r.jpg",
+            "https://res.cloudinary.com/dfk4z2ch2/image/upload/v1706478085/HolyFamily/HFExterior/eyfcljmchl4ukylb110l.jpg",
+            "https://res.cloudinary.com/dfk4z2ch2/image/upload/v1703065465/HolyFamily/HFExterior/msym8lvsefo2b37kiqfx.jpg"
+          ],
+          "Captions": [
+            "Car Park",
+            "FootBall Pitch and training on going",
+            "A Bird's eye view of HFYV",
+            "A Side view of B-Block",
+            "Entrance to HFYV hostel",
+            "Bird's eye view of B-Block and A-Block",
+            "JohnWick on the ball",
+            "The Gen House",
+            "The Tennis Court",
+            "View of the HFYV hall and dining hall building"
+          ]
       },
 
     {
